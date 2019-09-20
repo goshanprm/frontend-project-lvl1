7064a705b844f231b7a3529a16798147cd50988b
Egor Batrakov <goshanprm@gmail.com>
2019-09-20 17:34:50 +0500

    fixed eslint errors

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -4,4 +4,4 @@ import readlineSync from '..';
 console.log('Welcome to the Brain Games!\n');
 
 const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
+console.log(`Hello, ${userName}!`);
