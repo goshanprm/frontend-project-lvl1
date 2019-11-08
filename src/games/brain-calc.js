69b203fbfc50a7c470d47d52c1451702da12ebdc
Egor Batrakov <goshanprm@gmail.com>
2019-11-08 18:27:50 +0300

    updated brain-calc: deleted array

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -6,18 +6,20 @@ const descriptionOfGame = 'What is the result of the expression?';
 const generatedGameData = () => {
   const integer1 = getRandomInt(1, 100);
   const integer2 = getRandomInt(1, 100);
-  const operators = ['+', '-', '*'];
-  const randOperator = operators[Math.floor(Math.random() * (operators.length))];
-  const actualQuestion = `${integer1} ${randOperator} ${integer2}`;
+  const randOperator = getRandomInt(1, 3);
+  let actualQuestion = '';
   let actualAnswer = 0;
   switch (randOperator) {
-    case '+':
+    case 1:
+      actualQuestion = `${integer1} + ${integer2}`;
       actualAnswer = parseInt(integer1, 10) + parseInt(integer2, 10);
       break;
-    case '-':
+    case 2:
+      actualQuestion = `${integer1} - ${integer2}`;
       actualAnswer = parseInt(integer1, 10) - parseInt(integer2, 10);
       break;
     default:
+      actualQuestion = `${integer1} * ${integer2}`;
       actualAnswer = parseInt(integer1, 10) * parseInt(integer2, 10);
   }
   const gameData = cons(actualQuestion, actualAnswer);
