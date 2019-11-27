357cc96df010069b8e306b9bc215a7d85e9224e7
Egor Batrakov <goshanprm@gmail.com>
2019-11-27 19:18:05 +0300

    refactor(calc.js) delete magic number, delete unnecessary functions

--- src/games/calc.js
+++ src/games/calc.js
@@ -7,18 +7,19 @@ const descriptionOfGame = 'What is the result of the expression?';
 const minValue = 1;
 const maxValue = 100;
 const operators = '+-*';
+const operationsCount = 3;
 
 const calculateExpression = (a, b, operator) => {
   let expression;
   switch (operator) {
     case '+':
-      expression = parseInt(a, 10) + parseInt(b, 10);
+      expression = a + b;
       return expression;
     case '-':
-      expression = parseInt(a, 10) - parseInt(b, 10);
+      expression = a - b;
       return expression;
     case '*':
-      expression = parseInt(a, 10) * parseInt(b, 10);
+      expression = a * b;
       return expression;
     default:
       return false;
@@ -28,7 +29,7 @@ const calculateExpression = (a, b, operator) => {
 const generatedGameData = () => {
   const integer1 = getRandomInt(minValue, maxValue);
   const integer2 = getRandomInt(minValue, maxValue);
-  const randomOperator = operators[getRandomInt(0, 2)];
+  const randomOperator = operators[getRandomInt(0, operationsCount - 1)];
   const actualQuestion = `${integer1} ${randomOperator} ${integer2}`;
   const actualAnswer = calculateExpression(integer1, integer2, randomOperator);
   const gameData = cons(actualQuestion, actualAnswer);
